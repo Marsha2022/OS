95caa6645e2854e2a9079153071bed023eccab79
Marshal <113766056+Marsha2022@users.noreply.github.com>
2024-03-02 12:35:28 +0300

    Delete kp directory

--- kp/dataBase.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATABASE_H
-#define DATABASE_H
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> words_database{"agent","basic","cabin","earth","grave"};
-
-#endif //DATABASE_H
