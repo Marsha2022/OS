85296dc3d2de817c6b989e9dff30ed41894fb697
Marshal <113766056+Marsha2022@users.noreply.github.com>
2024-03-04 17:53:02 +0300

    Delete lab4 directory

--- lab4/include/utils.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-#include <dlfcn.h>
-
-using SinIntegralFunc = float (*)(float, float, float);
-using PrimeCountFunc = int (*)(int, int);
-
-void* LoadLibrary(const char *libraryName);
-void UnloadLibrary(void* handle);
