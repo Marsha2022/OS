1c962c321dfd001a87ab6f7a8622126e22122031
Marshal <113766056+Marsha2022@users.noreply.github.com>
2024-03-04 17:52:53 +0300

    Delete lab3 directory

--- lab3/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "parent.hpp"
-
-int main() {
-    ParentProcess(getenv("PATH_TO_CHILD"));
-    // bash: export PATH_TO_CHILD="/home/kristinab/ubuntu_main/OS_labs/build/lab3/child3"
-    // bash: printenv PATH_TO_CHILD
-    exit(EXIT_SUCCESS);
-}
